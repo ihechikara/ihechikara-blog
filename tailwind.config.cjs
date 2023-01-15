c5b3d03c42a6e0b54b970cc7aa6054ec05c9706a
Ihechikara <ihechivinabba@gmail.com>
2023-01-15 22:47:39 +0100

    Styled blog post page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {
-            width: {
-                'fit-content': 'fit-content',
-            },
-        },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      width: {
+        "fit-content": "fit-content",
+      },
     },
-    plugins: [],
-}
+  },
+    plugins: [
+        require("@tailwindcss/typography"),
+    ],
+};
