28f2b55c717a0f648a9e85456cc3878b81981c27
Ihechikara <ihechivinabba@gmail.com>
2023-03-18 20:18:29 +0100

    added Google analytics 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown'
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   // output: 'server',
-  site: 'https://ihechikara.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://ihechikara.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
