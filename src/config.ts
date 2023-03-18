f48c2074ee394312a0343e0c97b8aa09610e01f2
Ihechikara <ihechivinabba@gmail.com>
2023-03-18 19:45:09 +0100

    changed newsletter form position and edited meta description for blog

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Ihechikara Abba";
+export const SITE_DESCRIPTION = "Ihechikara's blog";
 export const HOME_PAGE = '/';
 export const BLOG_HOME_PAGE = '/blog';
